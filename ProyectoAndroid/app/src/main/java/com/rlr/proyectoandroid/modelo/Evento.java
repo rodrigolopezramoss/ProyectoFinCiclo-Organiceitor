df439765e6137d54a095e21eb049fdc4a01e308e
Rodrigo López Ramos <55782190+rodrigolopezramoss@users.noreply.github.com>
2021-05-14 23:27:40 +0200

    Add files via upload

---
+++ ProyectoAndroid/app/src/main/java/com/rlr/proyectoandroid/modelo/Evento.java
@@ -0,0 +1,12 @@
+package com.rlr.proyectoandroid.modelo;
+
+public class Evento {
+    private String nombre;
+    private String tipo;
+    private String categoría;
+    private String descripcion;
+
+    private boolean contratacion;
+    private boolean invitacion;
+    private boolean publicidad;
+}
---
+++ ProyectoAndroid/app/src/main/java/com/rlr/proyectoandroid/recyclerview/MiViewHolder.java
@@ -0,0 +1,4 @@
+package com.rlr.proyectoandroid.recyclerview;
+
+public class MiViewHolder {
+}
